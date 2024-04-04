f40dde5785d6e67c4afeef1771921c6a948e08d9
Gabe <gabriel.tyler16@gmail.com>
2024-04-03 20:41:55 -0500

    Fix write-tree

--- src/commands/write_tree.rs
+++ src/commands/write_tree.rs
@@ -1,16 +1,31 @@
 use anyhow::Context;
 use std::io::Cursor;
 use std::os::unix::fs::PermissionsExt;
-use std::{fs, os::unix::fs::MetadataExt, path::Path};
+use std::{fs, path::Path};
 
 use crate::objects::{Kind, Object};
 
 fn write_tree_for(path: &Path) -> anyhow::Result<Option<[u8; 20]>> {
     let mut dir =
         fs::read_dir(path).with_context(|| format!("open directory {}", path.display()))?;
-    let mut tree_object = Vec::new();
+
+    let mut entries = Vec::new();
     while let Some(entry) = dir.next() {
         let entry = entry.with_context(|| format!("bad directory entry in {}", path.display()))?;
+        entries.push(entry);
+    }
+    entries.sort_unstable_by(|a, b| {
+        let mut afn = a.file_name().into_encoded_bytes();
+        let mut bfn = b.file_name().into_encoded_bytes();
+        // git considers longer string to come before shorter strings,
+        // which means the terminating char is high (0xff) not low (0x00)
+        afn.push(0xff);
+        bfn.push(0xff);
+        afn.cmp(&bfn)
+    });
+
+    let mut tree_object = Vec::new();
+    for entry in entries {
         let file_name = entry.file_name();
         if file_name == ".git" {
             // skip the .git dir
@@ -35,23 +50,23 @@ fn write_tree_for(path: &Path) -> anyhow::Result<Option<[u8; 20]>> {
             };
             hash
         } else {
-            // let hash = Object::blob_from_file(&path)
-            //     .context("open blob input file")?
-            //     .write_to_objects()
-            //     .context("stream object into file")?;
-            let tmp = "temporary"; // ideally random name
             let hash = Object::blob_from_file(&path)
                 .context("open blob input file")?
-                .write(fs::File::create(tmp).context("construct temp file for blob")?)
-                .context("stream file into blob")?;
-            let hash_hex = hex::encode(hash);
-            fs::create_dir_all(format!(".git/objects/{}/", &hash_hex[..2]))
-                .context("create subdir of .git/objects")?;
-            fs::rename(
-                tmp,
-                format!(".git/objects/{}/{}", &hash_hex[..2], &hash_hex[2..]),
-            )
-            .context("move blob file into .git/objects")?;
+                .write_to_objects()
+                .context("stream object into file")?;
+            // let tmp = "temporary"; // ideally random name
+            // let hash = Object::blob_from_file(&path)
+            //     .context("open blob input file")?
+            //     .write(fs::File::create(tmp).context("construct temp file for blob")?)
+            //     .context("stream file into blob")?;
+            // let hash_hex = hex::encode(hash);
+            // fs::create_dir_all(format!(".git/objects/{}/", &hash_hex[..2]))
+            //     .context("create subdir of .git/objects")?;
+            // fs::rename(
+            //     tmp,
+            //     format!(".git/objects/{}/{}", &hash_hex[..2], &hash_hex[2..]),
+            // )
+            // .context("move blob file into .git/objects")?;
             hash
         };
         tree_object.extend(mode.as_bytes());
