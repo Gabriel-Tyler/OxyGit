654b6fcfdebb8445e735083f4c7e30bbf146f027
Gabe <gabriel.tyler16@gmail.com>
2024-04-03 23:15:24 -0500

    Fix ordering of write-tree output

--- src/commands/write_tree.rs
+++ src/commands/write_tree.rs
@@ -1,4 +1,5 @@
 use anyhow::Context;
+use std::cmp::Ordering;
 use std::io::Cursor;
 use std::os::unix::fs::PermissionsExt;
 use std::{fs, path::Path};
@@ -12,27 +13,46 @@ fn write_tree_for(path: &Path) -> anyhow::Result<Option<[u8; 20]>> {
     let mut entries = Vec::new();
     while let Some(entry) = dir.next() {
         let entry = entry.with_context(|| format!("bad directory entry in {}", path.display()))?;
-        entries.push(entry);
+        let name = entry.file_name();
+        let meta = entry.metadata().context("metadata for directory entry")?;
+        entries.push((entry, name, meta));
     }
-    entries.sort_unstable_by(|a, b| {
-        let mut afn = a.file_name().into_encoded_bytes();
-        let mut bfn = b.file_name().into_encoded_bytes();
-        // git considers longer string to come before shorter strings,
-        // which means the terminating char is high (0xff) not low (0x00)
-        afn.push(0xff);
-        bfn.push(0xff);
-        afn.cmp(&bfn)
+    entries.sort_unstable_by(|(_, a_name, a_meta), (_, b_name, b_meta)| {
+        // https://github.com/git/git/blob/master/tree.c#L99
+        let afn = a_name.to_owned().into_encoded_bytes();
+        let bfn = b_name.to_owned().into_encoded_bytes();
+        let common_len = std::cmp::min(afn.len(), bfn.len());
+        match afn[..common_len].cmp(&bfn[..common_len]) {
+            Ordering::Equal => {}
+            o => return o,
+        }
+        if afn.len() == bfn.len() {
+            return Ordering::Equal;
+        }
+        let c1 = if let Some(c) = afn.get(common_len).copied() {
+            Some(c)
+        } else if a_meta.is_dir() {
+            Some(b'/')
+        } else {
+            None
+        };
+        let c2 = if let Some(c) = bfn.get(common_len).copied() {
+            Some(c)
+        } else if b_meta.is_dir() {
+            Some(b'/')
+        } else {
+            None
+        };
+        c1.cmp(&c2)
     });
 
     let mut tree_object = Vec::new();
-    for entry in entries {
-        let file_name = entry.file_name();
+    for (entry, name, meta) in entries {
         // also program looking through .gitignore
-        if file_name == ".git" || file_name == "target" {
+        if name == ".git" || name == "target" {
             // skip the .git dir
             continue;
         }
-        let meta = entry.metadata().context("metadata for directory entry")?;
         let mode = if meta.is_dir() {
             "40000"
         } else if meta.is_symlink() {
@@ -59,7 +79,7 @@ fn write_tree_for(path: &Path) -> anyhow::Result<Option<[u8; 20]>> {
         };
         tree_object.extend(mode.as_bytes());
         tree_object.push(b' ');
-        tree_object.extend(file_name.as_encoded_bytes());
+        tree_object.extend(name.as_encoded_bytes());
         tree_object.push(b'\0');
         tree_object.extend(hash);
     }
